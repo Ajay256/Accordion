9453083b04aff01e60ade10e636e0d5cc7280557
Ajay256 <ajayverma4559922@gmail.com>
2024-04-23 14:48:39 +0530

    First commit

---
+++ src/components/accordian/data.js
@@ -0,0 +1,27 @@
+const data = [
+    {
+        id: '1',
+        question: "What are accordion components",
+        answer: "The wrapper for the Accordion header, which expands or collapses the content when clicked"
+    },
+
+    {
+        id: '2',
+        question: "What is React",
+        answer: "React is open source frontend javascript framework."
+    },
+
+    {
+        id: '3',
+        question: "How to start new react project using Vite",
+        answer: "Open CMD and type command npm create vite@latest after the give project name and after this reach inside project folder type command npm install"
+    },
+
+    {
+        id: '4',
+        question: "What is the concept we use in this project",
+        answer: "In this we use Hooks like useState, props, components etc"
+    }
+];
+
+export default data;
